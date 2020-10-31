8e59d4eccb833154c946a0edc6ac5b926ba4eb66
Léo Pons <leo.pons@wanadoo.fr>
2020-10-31 17:52:22 +0100

    setup db and models

--- upsets/models.py
+++ upsets/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    tag = models.CharField(max_length=200)
+
+
+class Tournament(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    start_date = models.DateField()
+    name = models.CharField(max_length=200)
+
+
+class Set(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    winner = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name='winner')
+    looser = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name='looser')
+    winner_score = models.IntegerField()
+    looser_score = models.IntegerField()
+    round_name = models.CharField(max_length=200)
+    best_of = models.IntegerField(null=True, blank=True)
